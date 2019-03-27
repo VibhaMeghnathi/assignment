e23f0762b1f871197256c93498a46c8568f28b15
Vibha <46936370+VibhaMeghnathi@users.noreply.github.com>
2019-03-27 23:30:37 +0530

    file updated

--- src/main/java/com/assignment/htmscrapper/HtmScrapperApplication.java
+++ src/main/java/com/assignment/htmscrapper/HtmScrapperApplication.java
@@ -1,6 +1,8 @@
 package com.assignment.htmscrapper;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class HtmScrapperApplication {
 public static void main(String[] args) {
