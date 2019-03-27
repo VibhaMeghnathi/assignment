0e610f0c1a8cafcaf76069a296a07bd251c7500d
Vibha <46936370+VibhaMeghnathi@users.noreply.github.com>
2019-03-27 23:38:49 +0530

    file updated

--- src/main/java/com/assignment/htmscrapper/scrap/service/HtmlScrapperService.java
+++ src/main/java/com/assignment/htmscrapper/scrap/service/HtmlScrapperService.java
@@ -1,6 +1,9 @@
 package com.assignment.htmscrapper.scrap.service;
 
 import com.assignment.htmscrapper.scrap.pojo.Article;
+import java.util.List;
+import java.util.Set;
+
 
 public interface HtmlScrapperService {
 
