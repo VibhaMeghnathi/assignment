76038ef382492bdab8feb8ac213c681d5b5c6429
Vibha <46936370+VibhaMeghnathi@users.noreply.github.com>
2019-03-27 22:36:35 +0530

    main file uploaded

---
+++ src/main/java/com/assignment/htmscrapper/HtmScrapperApplication.java
@@ -0,0 +1,9 @@
+package com.assignment.htmscrapper;
+
+import org.springframework.boot.SpringApplication;
+@SpringBootApplication
+public class HtmScrapperApplication {
+public static void main(String[] args) {
+        SpringApplication.run(HtmScrapperApplication.class, args);
+    }
+}
